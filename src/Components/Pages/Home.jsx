9d054960b595d11d053640db4086283f8457e7ea
MarzukhAsjad <marz.asjad00@gmail.com>
2024-07-17 23:54:52 +0800

    Refactor: Place button on top so that posts clutter do not affect its position

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -19,14 +19,14 @@ const Home = () => {
         <div className="flex-auto w-[60%] absolute left-[20%] top-14 bg-gray-100 rounded-xl">
           <div className="w-[80%] mx-auto">
             <CardSection></CardSection>
+            <div className="flex justify-center items-center mt-4 mb-8">
+              <Link to="/studio" className="inline-block">
+                <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
+                  Go to Designer Studio
+                </button>
+              </Link>
+            </div>
             <Main></Main>
-          </div>
-          <div className="flex justify-center items-center mt-4 mb-8">
-            <Link to="/studio" className="inline-block">
-              <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
-                Go to Designer Studio
-              </button>
-            </Link>
           </div>
         </div>
         <div className="flex-auto w-[20%] fixed right-0 top-12">
