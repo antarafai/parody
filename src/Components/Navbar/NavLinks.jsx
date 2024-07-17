12a78ec085935bf9b0ba641e6a412c5decd7b671
Antara <antara.hasan@area28.io>
2024-07-18 01:04:44 +0800

    added link to design studio

--- src/Components/Navbar/NavLinks.jsx
+++ src/Components/Navbar/NavLinks.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NavLinks = () => {
   return (
     <div className="flex justify-center items-center cursor-pointer">
       <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500">
         <svg
+          id="home"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -21,6 +23,7 @@ const NavLinks = () => {
       </div>
       <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500">
         <svg
+          id="messages"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -35,24 +38,28 @@ const NavLinks = () => {
           />
         </svg>
       </div>
-      <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500"  onClick={() => {window.location.href = '/studio'}}>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6 "
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
-          />
-        </svg>
+      <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500">
+        <Link to="/studio" className="inline-block">
+          <svg
+            id="studio"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 "
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21 7.5l-2.25-1.313M21 7.5v2.25m0-2.25l-2.25 1.313M3 7.5l2.25-1.313M3 7.5l2.25 1.313M3 7.5v2.25m9 3l2.25-1.313M12 12.75l-2.25-1.313M12 12.75V15m0 6.75l2.25-1.313M12 21.75V19.5m0 2.25l-2.25-1.313m0-16.875L12 2.25l2.25 1.313M21 14.25v2.25l-2.25 1.313m-13.5 0L3 16.5v-2.25"
+            />
+          </svg>
+        </Link>
       </div>
       <div className="hover:translate-y-1 duration-500 ease-in-out hover:text-blue-500">
         <svg
+          id="awards"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
