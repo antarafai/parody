8e530dacaa9e76c73dd5fe595850822c5f9d5f2b
MarzukhAsjad <marz.asjad00@gmail.com>
2024-07-20 19:32:40 +0800

    Refactor: Separate WebGLRenderer component and Studio.jsx

--- src/Components/Pages/Studio.jsx
+++ src/Components/Pages/Studio.jsx
@@ -1,205 +1,22 @@
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import Stats from 'three/examples/jsm/libs/stats.module';
-import { GUI } from 'dat.gui';
+// Studio.jsx
+import React, { useRef } from 'react';
+import WebGLRenderer from '../WebGL/WebGLRenderer';
 
 const FBXAnimations = () => {
-  const mountRef = useRef(null);
   const progressBarRef = useRef(null);
-
-  useEffect(() => {
-    const mount = mountRef.current;
-    const progressBar = progressBarRef.current;
-
-    const scene = new THREE.Scene();
-    const axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
-
-    const light = new THREE.PointLight(0xffffff, 1000);
-    light.position.set(2.5, 7.5, 15);
-    scene.add(light);
-
-    const camera = new THREE.PerspectiveCamera(75, mount.clientWidth / mount.clientHeight, 0.1, 1000);
-    camera.position.set(0.8, 1.4, 1.0);
-
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(mount.clientWidth, mount.clientHeight);
-    mount.appendChild(renderer.domElement);
-
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.screenSpacePanning = true;
-    controls.target.set(0, 1, 0);
-
-    let mixer;
-    let modelReady = false;
-    let animationActions = [];
-    let activeAction;
-    let lastAction;
-
-    const fbxLoader = new FBXLoader();
-    fbxLoader.load(
-      '/models/Y-Bot-T-pose.fbx',
-      (object) => {
-        object.scale.set(0.01, 0.01, 0.01);
-        mixer = new THREE.AnimationMixer(object);
-        let animationAction = mixer.clipAction(object.animations[0]);
-        animationActions.push(animationAction);
-        animationsFolder.add(animations, 'default');
-        activeAction = animationActions[0];
-        scene.add(object);
-
-        fbxLoader.load(
-          '/models/Joyful-Jump.fbx',
-          (object) => {
-            console.log('loaded samba');
-            let animationAction = mixer.clipAction(object.animations[0]);
-            animationActions.push(animationAction);
-            animationsFolder.add(animations, 'samba');
-
-            fbxLoader.load(
-              '/models/Slide-Hip-Hop-Dance.fbx',
-              (object) => {
-                console.log('loaded bellydance');
-                let animationAction = mixer.clipAction(object.animations[0]);
-                animationActions.push(animationAction);
-                animationsFolder.add(animations, 'bellydance');
-
-                fbxLoader.load(
-                  '/models/Shoved-Reaction-With-Spin.fbx',
-                  (object) => {
-                    console.log('loaded goofyrunning');
-                    object.animations[0].tracks.shift();
-                    let animationAction = mixer.clipAction(object.animations[0]);
-                    animationActions.push(animationAction);
-                    animationsFolder.add(animations, 'goofyrunning');
-                    progressBar.style.display = 'none';
-                    modelReady = true;
-                  },
-                  (xhr) => {
-                    if (xhr.lengthComputable) {
-                      var percentComplete = (xhr.loaded / xhr.total) * 100;
-                      progressBar.value = percentComplete;
-                      progressBar.style.display = 'block';
-                    }
-                  },
-                  (error) => {
-                    console.log(error);
-                  }
-                );
-              },
-              (xhr) => {
-                if (xhr.lengthComputable) {
-                  var percentComplete = (xhr.loaded / xhr.total) * 100;
-                  progressBar.value = percentComplete;
-                  progressBar.style.display = 'block';
-                }
-              },
-              (error) => {
-                console.log(error);
-              }
-            );
-          },
-          (xhr) => {
-            if (xhr.lengthComputable) {
-              var percentComplete = (xhr.loaded / xhr.total) * 100;
-              progressBar.value = percentComplete;
-              progressBar.style.display = 'block';
-            }
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-      },
-      (xhr) => {
-        if (xhr.lengthComputable) {
-          var percentComplete = (xhr.loaded / xhr.total) * 100;
-          progressBar.value = percentComplete;
-          progressBar.style.display = 'block';
-        }
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-
-    function onWindowResize() {
-      camera.aspect = mount.clientWidth / mount.clientHeight;
-      camera.updateProjectionMatrix();
-      renderer.setSize(mount.clientWidth, mount.clientHeight);
-      render();
-    }
-    window.addEventListener('resize', onWindowResize, false);
-
-    const stats = new Stats();
-    document.body.appendChild(stats.dom);
-
-    var animations = {
-      default: function () {
-        setAction(animationActions[0]);
-      },
-      samba: function () {
-        setAction(animationActions[1]);
-      },
-      bellydance: function () {
-        setAction(animationActions[2]);
-      },
-      goofyrunning: function () {
-        setAction(animationActions[3]);
-      },
-    };
-
-    const setAction = (toAction) => {
-      if (toAction !== activeAction) {
-        lastAction = activeAction;
-        activeAction = toAction;
-        lastAction.fadeOut(1);
-        activeAction.reset();
-        activeAction.fadeIn(1);
-        activeAction.play();
-      }
-    };
-
-    const gui = new GUI();
-    const animationsFolder = gui.addFolder('Animations');
-    animationsFolder.open();
-
-    const clock = new THREE.Clock();
-
-    function animate() {
-      requestAnimationFrame(animate);
-      controls.update();
-      if (modelReady) mixer.update(clock.getDelta());
-      render();
-      stats.update();
-    }
-
-    function render() {
-      renderer.render(scene, camera);
-    }
-
-    animate();
-
-    return () => {
-      window.removeEventListener('resize', onWindowResize);
-      mount.removeChild(renderer.domElement);
-      document.body.removeChild(stats.dom);
-    };
-  }, []);
 
   return (
     <div className="flex flex-col h-screen">
       <div className="flex flex-row flex-1">
-        <div className="flex flex-col bg-gray-200 p-2 w-16">
-          <div className="flex-1 bg-green-500 m-1 rounded"></div>
-          <div className="flex-1 bg-green-500 m-1 rounded"></div>
-          <div className="flex-1 bg-green-500 m-1 rounded"></div>
-          <div className="flex-1 bg-green-500 m-1 rounded"></div>
-          <div className="flex-1 bg-green-500 m-1 rounded"></div>
+        <div id="sidePanel" className="flex flex-col bg-gray-200 p-2 w-16 fixed" style={{ maxHeight: '50vh' }}>
+          <button className="bg-green-500 mb-2 rounded h-10 w-10"></button>
+          <button className="bg-green-500 mb-2 rounded h-10 w-10"></button>
+          <button className="bg-green-500 mb-2 rounded h-10 w-10"></button>
+          <button className="bg-green-500 mb-2 rounded h-10 w-10"></button>
+          <button className="bg-green-500 mb-2 rounded h-10 w-10"></button>
         </div>
-        <div className="relative flex-grow bg-gray-300" ref={mountRef}>
+        <div className="relative flex-grow bg-gray-300 ml-16">
+          <WebGLRenderer progressBarRef={progressBarRef} />
           <progress value="0" max="100" id="progressBar" ref={progressBarRef} className="absolute top-2 left-2"></progress>
         </div>
       </div>
---
+++ src/Components/WebGL/WebGLRenderer.jsx
@@ -0,0 +1,192 @@
+// WebGLRenderer.jsx
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'dat.gui';
+
+const WebGLRenderer = ({ progressBarRef }) => {
+  const mountRef = useRef(null);
+
+  useEffect(() => {
+    const mount = mountRef.current;
+    const progressBar = progressBarRef.current;
+
+    const scene = new THREE.Scene();
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
+
+    const light = new THREE.PointLight(0xffffff, 1000);
+    light.position.set(2.5, 7.5, 15);
+    scene.add(light);
+
+    const camera = new THREE.PerspectiveCamera(75, mount.clientWidth / mount.clientHeight, 0.1, 1000);
+    camera.position.set(0.8, 1.4, 1.0);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(mount.clientWidth, mount.clientHeight);
+    mount.appendChild(renderer.domElement);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.screenSpacePanning = true;
+    controls.target.set(0, 1, 0);
+
+    let mixer;
+    let modelReady = false;
+    let animationActions = [];
+    let activeAction;
+    let lastAction;
+
+    const fbxLoader = new FBXLoader();
+    fbxLoader.load(
+      '/models/Y-Bot-T-pose.fbx',
+      (object) => {
+        object.scale.set(0.01, 0.01, 0.01);
+        mixer = new THREE.AnimationMixer(object);
+        let animationAction = mixer.clipAction(object.animations[0]);
+        animationActions.push(animationAction);
+        animationsFolder.add(animations, 'default');
+        activeAction = animationActions[0];
+        scene.add(object);
+
+        fbxLoader.load(
+          '/models/Joyful-Jump.fbx',
+          (object) => {
+            let animationAction = mixer.clipAction(object.animations[0]);
+            animationActions.push(animationAction);
+            animationsFolder.add(animations, 'samba');
+
+            fbxLoader.load(
+              '/models/Slide-Hip-Hop-Dance.fbx',
+              (object) => {
+                let animationAction = mixer.clipAction(object.animations[0]);
+                animationActions.push(animationAction);
+                animationsFolder.add(animations, 'bellydance');
+
+                fbxLoader.load(
+                  '/models/Shoved-Reaction-With-Spin.fbx',
+                  (object) => {
+                    object.animations[0].tracks.shift();
+                    let animationAction = mixer.clipAction(object.animations[0]);
+                    animationActions.push(animationAction);
+                    animationsFolder.add(animations, 'goofyrunning');
+                    progressBar.style.display = 'none';
+                    modelReady = true;
+                  },
+                  (xhr) => {
+                    if (xhr.lengthComputable) {
+                      var percentComplete = (xhr.loaded / xhr.total) * 100;
+                      progressBar.value = percentComplete;
+                      progressBar.style.display = 'block';
+                    }
+                  },
+                  (error) => {
+                    console.log(error);
+                  }
+                );
+              },
+              (xhr) => {
+                if (xhr.lengthComputable) {
+                  var percentComplete = (xhr.loaded / xhr.total) * 100;
+                  progressBar.value = percentComplete;
+                  progressBar.style.display = 'block';
+                }
+              },
+              (error) => {
+                console.log(error);
+              }
+            );
+          },
+          (xhr) => {
+            if (xhr.lengthComputable) {
+              var percentComplete = (xhr.loaded / xhr.total) * 100;
+              progressBar.value = percentComplete;
+              progressBar.style.display = 'block';
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      },
+      (xhr) => {
+        if (xhr.lengthComputable) {
+          var percentComplete = (xhr.loaded / xhr.total) * 100;
+          progressBar.value = percentComplete;
+          progressBar.style.display = 'block';
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+
+    function onWindowResize() {
+      camera.aspect = mount.clientWidth / mount.clientHeight;
+      camera.updateProjectionMatrix();
+      renderer.setSize(mount.clientWidth, mount.clientHeight);
+      render();
+    }
+    window.addEventListener('resize', onWindowResize, false);
+
+    const stats = new Stats();
+    document.body.appendChild(stats.dom);
+
+    var animations = {
+      default: function () {
+        setAction(animationActions[0]);
+      },
+      samba: function () {
+        setAction(animationActions[1]);
+      },
+      bellydance: function () {
+        setAction(animationActions[2]);
+      },
+      goofyrunning: function () {
+        setAction(animationActions[3]);
+      },
+    };
+
+    const setAction = (toAction) => {
+      if (toAction !== activeAction) {
+        lastAction = activeAction;
+        activeAction = toAction;
+        lastAction.fadeOut(1);
+        activeAction.reset();
+        activeAction.fadeIn(1);
+        activeAction.play();
+      }
+    };
+
+    const gui = new GUI();
+    const animationsFolder = gui.addFolder('Animations');
+    animationsFolder.open();
+
+    const clock = new THREE.Clock();
+
+    function animate() {
+      requestAnimationFrame(animate);
+      controls.update();
+      if (modelReady) mixer.update(clock.getDelta());
+      render();
+      stats.update();
+    }
+
+    function render() {
+      renderer.render(scene, camera);
+    }
+
+    animate();
+
+    return () => {
+      window.removeEventListener('resize', onWindowResize);
+      mount.removeChild(renderer.domElement);
+      document.body.removeChild(stats.dom);
+    };
+  }, [progressBarRef]);
+
+  return <div ref={mountRef} style={{ width: '100%', height: '100%' }} />;
+};
+
+export default WebGLRenderer;
